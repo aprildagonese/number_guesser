65f3d799505871e8c6dc886f7b09efd34d27c683
April Dagonese <adagonese@gmail.com>
2019-04-19 12:17:24 -0600

    Adds wrapper to fully load and parse

--- server.js
+++ server.js
@@ -1,37 +1,41 @@
-const guessForm = document.querySelector('.guess-form')
-const guessBox = document.querySelector('.guess-box')
-const guessButton = document.querySelector('.guess-button')
-const clearButton = document.querySelector('.clear-button')
-const guess = document.querySelector('.guess')
-const result = document.querySelector('#result')
-const resetButton = document.querySelector('#reset-button')
-const secretNumber = Math.floor(Math.random() * 100) + 1;
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
 
-guessForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-});
-guessButton.addEventListener('click', clicked);
-clearButton.addEventListener('click', clear);
-resetButton.addEventListener('click', reset);
+  const guessForm = document.querySelector('.guess-form');
+  const guessBox = document.querySelector('.guess-box');
+  const guessButton = document.querySelector('#guess-button');
+  const clearButton = document.querySelector('#clear-button');
+  const guess = document.querySelector('.guess');
+  const result = document.querySelector('#result');
+  const resetButton = document.querySelector('#reset-button');
+  const secretNumber = Math.floor(Math.random() * 100) + 1;
+
+  guessForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+  });
+  guessButton.addEventListener('click', clicked);
+  clearButton.addEventListener('click', clear);
+  resetButton.addEventListener('click', reset);
 
 
-function clicked() {
-  var inputValue = parseInt(guessBox.value);
-  document.getElementById("info-text").innerHTML = "Your last guess was:";
-  document.getElementById("guess").innerHTML = inputValue;
-  if (inputValue > secretNumber) {
-    document.getElementById("result").innerHTML = "That is too high.";
-  } else if ( inputValue < secretNumber) {
-    document.getElementById("result").innerHTML = "That is too low.";
-  } else {
-    document.getElementById("result").innerHTML = "That is correct! You win!";
+  function clicked() {
+    var inputValue = parseInt(guessBox.value);
+    document.getElementById("info-text").innerHTML = "Your last guess was:";
+    document.getElementById("guess").innerHTML = inputValue;
+    if (inputValue > secretNumber) {
+      document.getElementById("result").innerHTML = "That is too high.";
+    } else if ( inputValue < secretNumber) {
+      document.getElementById("result").innerHTML = "That is too low.";
+    } else {
+      document.getElementById("result").innerHTML = "That is correct! You win!";
+    }
   }
-}
 
-function clear() {
-  guessForm.reset(true);
-}
+  function clear() {
+    guessForm.reset(true);
+  }
 
-function reset() {
-  location.reload(true);
-}
+  function reset() {
+    location.reload(true);
+  }
+});
